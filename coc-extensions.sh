9cce1e25341e1fab589b38470bc297f898090dc6
Med Aduh <im.ramidem@gmail.com>
2020-08-29 21:48:54 +0800

    remove coc-eslint

--- coc-extensions.sh
+++ coc-extensions.sh
@@ -11,4 +11,4 @@ then
   echo '{"dependencies":{}}'> package.json
 fi
 # Change extension names to the extensions you need
-npm install coc-python coc-tsserver coc-html coc-css coc-json coc-emmet coc-prettier coc-git coc-fzf-preview coc-eslint coc-cssmodules coc-vimlsp coc-phpls --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+npm install coc-python coc-tsserver coc-html coc-css coc-json coc-emmet coc-prettier coc-git coc-fzf-preview coc-cssmodules coc-vimlsp coc-phpls --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
